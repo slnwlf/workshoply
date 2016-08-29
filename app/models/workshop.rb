f6ab8fe56c4976b8e13023759f666c911fb052f0
tkhuynh <thequest2311@yahoo.com>
2016-08-29 13:29:54 -0700

    add default workshop image in asset image

--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -8,7 +8,7 @@ class Workshop < ActiveRecord::Base
 	validates :description, presence: true
 	validates :location, presence: true
 
-  has_attached_file :image, styles: { medium: ["600x300>", :png], thumb: ["400x200>", :png] }, default_url: "http://www.humanecology.rutgers.edu/images/facfaces/NO-IMAGE-AVAILABLE.jpg"
+  has_attached_file :image, styles: { medium: ["600x300>", :png], thumb: ["400x200>", :png] }, default_url: ActionController::Base.helpers.asset_path('workshop.jpg')
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end

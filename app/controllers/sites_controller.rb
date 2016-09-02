f9bd4f4dc0325d3cd6c6a3237a17ffa677c4ed4b
SBRKS <sbrks3@gmail.com>
2016-09-02 10:50:08 -0700

    featured workshops connect to db

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,7 +1,9 @@
 class SitesController < ApplicationController
   def index
   	@workshops = Workshop.all
-  	@workshop = Workshop.find(params[:id])
+  	@workshop1 = Workshop.find(1)
+  	@workshop2 = Workshop.find(3)
+  	@workshop3 = Workshop.find(5)
   end
 
   def about

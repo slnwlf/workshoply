154cf23c8808518e34fd0b9611efe01363d269fe
SBRKS <sbrks3@gmail.com>
2016-09-29 09:09:53 -0500

    changes user and talk flash message wording

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,9 +12,9 @@ class UsersController < ApplicationController
 	def update
 		if @user.update_attributes(user_params)
 			if also_change_email?
-				flash[:notice] = "Successfully update profile. You will receive an email to confirm your new email."
+				flash[:notice] = "Your profile was successfully updated. You will receive an email to confirm your new email."
 			else
-				flash[:notice] = "Successfully update profile."
+				flash[:notice] = "Your profile was successfully updated."
 			end
 			redirect_to user_path(current_user)
 		else

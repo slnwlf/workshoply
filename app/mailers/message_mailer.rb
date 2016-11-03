af83f8af60eef3839318a526a7c0aa3167c3f155
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2016-11-03 11:41:34 -0700

    making default contact info go do dave sloan at gmail

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,6 +1,6 @@
 class MessageMailer < ApplicationMailer
 
-	default :to => "info@bigtalker.io"
+	default :to => "dave.sloan@gmail.com"
 	def message_me(msg)
 		@msg = msg
 		mail from: @msg.email, subject: @msg.subject, body: @msg.content

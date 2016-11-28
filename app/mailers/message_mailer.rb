2f5989d21d425d464cb4e888b12c42b3e87df960
laurakathleen <laurakwalley@gmail.com>
2016-11-28 15:58:53 -0800

    changes default email to dave@bigtalker.io

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,6 +1,6 @@
 class MessageMailer < ApplicationMailer
 
-	default :to => "dave.sloan@gmail.com"
+	default :to => "dave@bigtalker.io"
 	def message_me(msg)
 		@msg = msg
 		mail from: @msg.email, subject: @msg.subject, body: @msg.content

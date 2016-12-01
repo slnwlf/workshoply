8d1f3303c4b0eaf3dfc69405855c487b6c867492
laurakathleen <laurakwalley@gmail.com>
2016-11-30 16:11:23 -0800

    replaces number_field with text_field & gets rid of :goal

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -15,7 +15,7 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-  	params.require(:message).permit(:name, :email, :subject, :content, :company,  :location, :number_participants, :budget, :goal, :audience, :date)
+  	params.require(:message).permit(:name, :email, :subject, :content, :company,  :location, :number_participants, :budget, :audience, :date)
   end
 
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message
 	include ActiveModel::Model
-	attr_accessor :name, :email, :subject, :content, :company, :location, :number_participants, :budget, :goal, :audience, :date
+	attr_accessor :name, :email, :subject, :content, :company, :location, :number_participants, :budget, :audience, :date
 	validates :name, :email, :subject, :content, presence: true
 
 end

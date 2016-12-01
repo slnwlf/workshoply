ed58a5ca8ee27ba21959e043fda79cecb4bbcf70
laurakathleen <laurakwalley@gmail.com>
2016-12-01 11:13:49 -0800

    adds admin attribute to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161019062904) do
+ActiveRecord::Schema.define(version: 20161201173603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -131,6 +131,20 @@ ActiveRecord::Schema.define(version: 20161019062904) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "speaker_requests", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "company"
+    t.string   "location"
+    t.string   "goal"
+    t.string   "audience"
+    t.date     "date"
+    t.integer  "budget"
+    t.integer  "number_attendees"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
   create_table "topics", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -138,12 +152,12 @@ ActiveRecord::Schema.define(version: 20161019062904) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
@@ -152,8 +166,8 @@ ActiveRecord::Schema.define(version: 20161019062904) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "full_name"
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
@@ -164,6 +178,7 @@ ActiveRecord::Schema.define(version: 20161019062904) do
     t.text     "bio"
     t.string   "organization"
     t.string   "link_to_bio"
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree

972f10155557fc668adad76175e1a34c3392a99a
tkhuynh <thequest2311@yahoo.com>
2016-12-05 14:06:37 -0800

    facebook share on talk

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,29 +19,29 @@
 
 /* Making titleize function for String */
 String.prototype.capitalize = function() {
-    return this.charAt(0).toUpperCase() + this.slice(1);
+  return this.charAt(0).toUpperCase() + this.slice(1);
 };
 String.prototype.titleize = function() {
   var string_array = this.split(' ');
   string_array = string_array.map(function(str) {
-     return str.capitalize(); 
+    return str.capitalize();
   });
-  
+
   return string_array.join(' ');
 };
 /**************************************/
 
 $(function() {
-	// city autocomplete
-	var options = {
-		types: ['(cities)'],
-		componentRestrictions: {
-			country: "us"
-		}
-	};
-	if ($(".location-input")[0]) {
-		var autocomplere = new google.maps.places.Autocomplete($(".location-input")[0], options);
-	}
+  // city autocomplete
+  var options = {
+    types: ['(cities)'],
+    componentRestrictions: {
+      country: "us"
+    }
+  };
+  if ($(".location-input")[0]) {
+    var autocomplere = new google.maps.places.Autocomplete($(".location-input")[0], options);
+  }
 
   // notice and error disappears in 10 seconds
   $(".notice-error-msg .alert").fadeTo(10000, 1000).fadeOut(1000, function() {
@@ -50,6 +50,31 @@ $(function() {
 
   $("button#signup-with-email, button#login-with-email").on("click", function() {
     $(this).addClass("hidden");
+  });
+
+  // facebook sharing
+  var fbid, channelUrl;
+  if (window.location.host == 'localhost:3000') {
+    fbid = "1000870533350913";
+  } else if (window.location.host == 'www.bigtalker.io') {
+    fbid = "751608544991088";
+  }
+  FB.init({
+    appId: fbid,
+    channelUrl: '//' + window.location.host,
+    status: true,
+    cookie: true,
+    xfbml: true,
+    version: 'v2.6'
+  });
+  $(".fb-share").on("click", function() {
+    FB.ui({
+      method: 'share',
+      display: 'popup',
+      mobile_iframe: true,
+      href: share_url,
+      hashtag: "#bigtalker"
+    }, function(response) {});
   });
 });
 
